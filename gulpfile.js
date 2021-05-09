1ba4ed18b509762c0bbce9de9e0baffccdb923de
moxemum <brinking1999@gmail.com>
2021-05-09 10:51:03 +0300

    homework #4 complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-const { watch } = require('gulp')
-const gulp = require('gulp')
+
+const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')
 
 
-function runSass(cb) {
-    gulp.src('scss/**/*.scss')
-        .pipe( sass().on('error', console.log ) )
-        .pipe( gulp.dest('css/') )
-    cb()
+function runSass() {
+    return src(['app/scss/**/*.scss', '!app/scss/modules/**'])
+        .pipe( sass({ outputStyle: 'compressed' }).on('error', console.log ) )
+        .pipe( dest('app/css/') )
 }
 
 
 exports.default = function() {
-    watch('scss/**/*.scss', runSass)
+    watch('app/scss/**/*.scss', runSass)
 }
